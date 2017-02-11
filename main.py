cf6d5f9d6ca7f488833bebc2ccb4bf7c59db0026
Ahsan Shahid <ahsanshahidcool@gmail.com>
2017-02-11 17:35:31 -0500

    predictions added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pandas
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt 
+from matplotlib import style
 
 #loading dataframe
 df = quandl.get("YAHOO/GOOGL")
@@ -14,24 +16,26 @@ df = df[['Open','High','Low','Adjusted Close','Volume']]
 df['HL_PCT'] = (df['High'] - df['Low']) / df['Adjusted Close'] * 100.0
 df['PCT_change'] = (df['Adjusted Close'] - df['Open']) / df['Open'] * 100.0
 
-#creating new data frame with features
+#Creating new data frame with features
 df = df[['Adjusted Close','HL_PCT','PCT_change','Volume']]
 
 forecast_col = 'Adjusted Close'
 df.fillna(-9999999, inplace=True)
 
-#Rounding up to a whole number, forecasting 30 days out
+#Rounding up to a whole number
 forecast_out = int(math.ceil(0.01*len(df)))
-print(forecast_out)
 
 #Defining label
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
@@ -41,4 +45,8 @@ clf = LinearRegression()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately);
+
+print(forecast_set, accuracy, forecast_out)
+
+
